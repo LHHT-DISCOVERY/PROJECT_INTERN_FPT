75f6e8acea287f1a5740066b0e87bd688d711ef4
LHHT-DISCOVERY <lytri102@gmail.com>
2023-03-29 20:51:56 +0700

    update task intern fpt software

--- managerment_player_footbal/src/main/java/com/example/managerment_player_footbal/service/CooachService/IPlayerRating.java
+++
@@ -1,9 +0,0 @@
-package com.example.managerment_player_footbal.service.CooachService;
-
-import com.example.managerment_player_footbal.model.PlayerRating;
-
-public interface IPlayerRating {
-
-    PlayerRating save(PlayerRating player);
-    PlayerRating getByCoachIdAndPlayerId(int playerId, int coachId);
-}
